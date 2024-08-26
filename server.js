5d1d1c7988b69f882e11ef728134a974b79a45e0
Zheka Grem <odinked@gmail.com>
2024-08-26 19:10:27 +0300

    add cors

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const crypto = require("crypto");
+const cors = require("cors");
 const bodyParser = require("body-parser");
+
+app.use(cors());
+
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
