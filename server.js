f267b3302640547634eca25049cdd62a863fee6b
Zheka Grem <odinked@gmail.com>
2024-08-26 19:24:07 +0300

    fix bug add cors02

--- server.js
+++ server.js
@@ -24,19 +24,26 @@ app.get("/", (req, res) => {
     order_id: Date.now().toString(),
   };
 
-  const data = Buffer.from(JSON.stringify(params)).toString("base64");
+  app.post("/", (req, res) => {
+  const { amount, description, order_id } = req.body;
+
+  const params = {
+    public_key: publicKey,
+    version: "3",
+    action: "pay",
+    amount: amount.toString(),
+    currency: "UAH",
+    description: description,
+    order_id: order_id,
+  };
+  
+   const data = Buffer.from(JSON.stringify(params)).toString("base64");
   const signature = crypto
     .createHmac("sha1", privateKey)
     .update(data)
     .digest("base64");
 
-  res.send(`
-    <form method="POST" action="https://www.liqpay.ua/api/3/checkout" accept-charset="utf-8">
-      <input type="hidden" name="data" value="${data}" />
-      <input type="hidden" name="signature" value="${signature}" />
-      <input type="submit" value="Оплатити" />
-    </form>
-  `);
+  res.json({ data, signature });
 });
 
 app.post("/callback", (req, res) => {
@@ -63,4 +70,4 @@ app.post("/callback", (req, res) => {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Сервер запущено на порту ${PORT}`);
-});
+})
